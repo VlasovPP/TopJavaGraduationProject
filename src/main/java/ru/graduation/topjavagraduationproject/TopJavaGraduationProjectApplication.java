46bd620c59db5d7be28f112ee4c3916281623b9a
Власов Павел Павлович <vlasovolder@yandex.ru>
2022-04-29 09:46:27 +0700

    Initial commit

---
+++ src/main/java/ru/graduation/topjavagraduationproject/TopJavaGraduationProjectApplication.java
@@ -0,0 +1,14 @@
+package ru.graduation.topjavagraduationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TopJavaGraduationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopJavaGraduationProjectApplication.class, args);
+    }
+
+}
